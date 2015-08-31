a96a33b50217ebc092f244430a6a21a8e1e0b839
Rick Hightower <hightower_r@apple.com>
2015-08-31 10:23:45 -0700

    adding application plugin to make the examples runnable.

--- todo/src/main/java/com/mammatustech/todo/HttpClient.java
+++ todo/src/main/java/com/mammatustech/todo/HttpClient.java
@@ -10,7 +10,8 @@ public class HttpClient {
         for (int index = 0; index < 100; index++) {
 
             HTTP.postJSON("http://localhost:8888/v1/todo-service/todo",
-                    JsonFactory.toJson(new Todo("name" + index, "desc" + index, System.currentTimeMillis() )));
+                    JsonFactory.toJson(new Todo("name" + index,
+                            "desc" + index, System.currentTimeMillis() )));
             System.out.print(".");
         }
     }
